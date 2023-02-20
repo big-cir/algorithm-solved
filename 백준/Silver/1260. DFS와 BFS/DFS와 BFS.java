364c27080f6d6af95a8f037fcf1b92238180f936
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-02-21 03:53:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 344 ms, Memory: 20480 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,43 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    static int[][] graph;       // map 표시 배열
-    static int[] ch;            // 방문여부 표시 배열
     static int N, M, V;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> graph;
+    static Queue<Integer> queue;
 
-    public static void DFS(int start_vertex) {
-        if (ch[start_vertex] == 1) return;                // 이미 방문한 vertex 이면 메서드 종료
-        else {
-            ch[start_vertex] = 1;                         // 방문하지 않았다면 방문을 체크
-            System.out.print(start_vertex + " ");
-            for (int i = 1; i <= N; i++) {          // 방문하지 않은 vertex 를 탐지
-                if (graph[start_vertex][i] == 1) {
-                    DFS(i);
+    public static void DFS(int vertex) {
+        if (ch[vertex] == 1) {
+            return;
+        } else {
+            ch[vertex] = 1;
+            System.out.print(vertex + " ");
+            for (int i = 0; i < graph.get(vertex).size(); i++) {
+                int cur = graph.get(vertex).get(i);
+                if (ch[cur] == 0) {
+                    DFS(cur);
                 }
             }
         }
     }
 
-    public static void BFS(int start_vertex) {
-        Arrays.fill(ch, 0);
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start_vertex);
-        ch[start_vertex] = 1;
-
+    public static void BFS(int vertex) {
+        ch[vertex] = 1;
         while (!queue.isEmpty()) {
             int cur = queue.poll();
             System.out.print(cur + " ");
-            for (int i = 1; i <= N; i++) {
-                if (ch[i] == 0 && graph[cur][i] == 1) {
-                    queue.offer(i);
-                    ch[i] = 1;
+
+            for (int i = 0; i < graph.get(cur).size(); i++) {
+                int next_vertex = graph.get(cur).get(i);
+                if (ch[next_vertex] == 0) {
+                    ch[next_vertex] = 1;
+                    queue.offer(next_vertex);
                 }
             }
         }
@@ -46,23 +43,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());   // 정점 개수
-        M = Integer.parseInt(st.nextToken());   // 간선 개수
-        V = Integer.parseInt(st.nextToken());   // 정점 번호
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-        graph = new int[N + 1][N + 1];
         ch = new int[N + 1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
-            graph[v1][v2] = 1;
-            graph[v2][v1] = 1;
+            graph.get(v1).add(v2);
+            graph.get(v2).add(v1);
+        }
+
+        // 번호가 작은 것 부터 방문 -> 오름차순 정렬
+        for (List<Integer> list : graph) {
+            Collections.sort(list);
         }
 
         DFS(V);
         System.out.println();
+
+        Arrays.fill(ch, 0);
+        queue = new LinkedList<>();
+        queue.offer(V);
         BFS(V);
     }
 }
