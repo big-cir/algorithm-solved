e37f175808dc0959caa2474121105c67b8bf78ad
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-03-20 19:15:06 +0900

    [level 1] Title: 모의고사, Time: 2.36 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,59 +1,20 @@
+import java.util.ArrayList;
 class Solution {
-    static int[] pattern_1 = {1, 2, 3, 4, 5};
-    static int[] pattern_2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    static int[] pattern_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-    public static int answer_pattern1(int[] answers) {
-        int count = 0;
-        int idx = 0;
-        for (int x : answers) {
-            if (idx == pattern_1.length) idx = 0;
-            if (x == pattern_1[idx++]) count++;
+    public int[] solution(int[] answer) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
-        return count;
-    }
-
-    public static int answer_pattern2(int[] answers) {
-        int count = 0;
-        int idx = 0;
-        for (int x : answers) {
-            if (idx == pattern_2.length) idx = 0;
-            if (x == pattern_2[idx++]) count++;
-        }
-        return count;
-
-    }
-
-    public static int answer_pattern3(int[] answers) {
-        int count = 0;
-        int idx = 0;
-        for (int x : answers) {
-            if (idx == pattern_3.length) idx = 0;
-            if (x == pattern_3[idx++]) count++;
-        }
-        return count;
-    }
-
-    public int[] solution(int[] answers) {
-        int max = Integer.MIN_VALUE;
-        int count1 = answer_pattern1(answers);
-        int count2 = answer_pattern2(answers);
-        int count3 = answer_pattern3(answers);
-
-        if (count1 == count2 && count2 == count3) {
-            return new int[] {1, 2, 3};
-        } else if (count1 == count2 && count1 > count3) {
-            return new int[] {1, 2};
-        } else if (count2 == count3 && count2 > count1) {
-            return new int[] {2, 3};
-        } else if (count1 == count3 && count1 > count2) {
-            return new int[] {1, 3};
-        } else {
-            max = Math.max(count1, Math.max(count2, count3));
-            if (max == count1) return new int[] {1};
-            if (max == count2) return new int[] {2};
-            if (max == count3) return new int[] {3};
-        }
-        return new int[]{};
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
