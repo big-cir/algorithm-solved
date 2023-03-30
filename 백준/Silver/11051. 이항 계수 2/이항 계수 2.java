47553e80f418b5f8d0d07154b9c7261a0c75222a
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-03-30 22:19:13 +0900

    [Silver II] Title: 이항 계수 2, Time: 136 ms, Memory: 18200 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] dp;
+    static int divNum = 10007;
+
+    public static int combi(int N, int K) {
+        if (dp[N][K] > 0) return dp[N][K];
+        if (N == K || K == 0) {
+            return 1;
+        } else {
+            return dp[N][K] = (combi(N - 1, K - 1) + combi(N - 1, K)) % divNum;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        dp = new int[N + 1][N + 1];
+
+        int answer = combi(N, K) ;
+        System.out.println(answer);
+    }
+}
