9c06a3368092b42697719371aab941ee642c464f
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-04-04 22:43:44 +0900

    [Silver I] Title: 기타 레슨, Time: 340 ms, Memory: 24340 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,10 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class Main {
+
+    public static int sub(int[] A, int mid) {
+        int blueRay = 1;
+        int sum = 0;
+
+        for (int x : A) {
+            if (sum + x> mid) {
+                blueRay++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return blueRay;
+    }
+
+    public static int binary(int[] A, int M) {
+        int answer = 0;
+        int lt = Arrays.stream(A).max().getAsInt();
+        int rt = Arrays.stream(A).sum();
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (sub(A, mid) <= M) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,38 +46,8 @@ public class Main {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(solution(N, M, A));
-    }
+        System.out.println(binary(A, M));
 
-    private static int solution(int N, int M, int[] A) {
-        int answer = 0;
-        int lt = Arrays.stream(A).max().getAsInt();
-        int rt = Arrays.stream(A).sum();
 
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if (count(A, mid) <= M) {
-                answer = mid;
-                rt = mid - 1;
-            } else {
-                lt = mid + 1;
-            }
-        }
-
-        return answer;
-    }
-
-    private static int count(int[] A, int capacity) {
-        int cnt = 1;  
-        int sum = 0;   
-        for (int x : A) {
-            if (sum + x > capacity) { 
-                cnt++;
-                sum = x;
-            } else {
-                sum += x;
-            }
-        }
-        return cnt;
     }
 }
