8ace120afab2e737899008e927353ef6313307b4
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-05-01 18:10:00 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 123708 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, sub;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void solution(int Level) {
+        if (Level == M) {
+            for (int x : sub) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sub[Level] = arr[i];
+            solution(Level + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        sub = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        solution(0);
+        System.out.println(sb.toString());
+    }
+}
