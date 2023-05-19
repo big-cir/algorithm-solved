73c5ff333d1fbc5424b5c4e4e8ee1cbd63b147f0
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-05-20 01:16:27 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] board = new int[100][100];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = y; j < y + 10; j++) {
+                for (int k = x; k < x + 10; k++) {
+                    board[j][k] = 1;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (board[i][j] == 1) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
