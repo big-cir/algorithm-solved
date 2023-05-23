d124695d04bd544c7c2b8d7ad87cece74c6e7fda
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-05-23 17:06:05 +0900

    [Silver IV] Title: 보물, Time: 136 ms, Memory: 14624 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] A, B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        Integer[] integers = Arrays.stream(B).boxed().toArray(Integer[]::new);
+        Arrays.sort(integers, Collections.reverseOrder());
+        B = Arrays.stream(integers).mapToInt(Integer::intValue).toArray();
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[i];
+        }
+        System.out.println(sum);
+    }
+}
