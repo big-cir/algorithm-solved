6cd7f33701863040b0cfba0f9f33d9e24d2df1c3
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-06-29 16:38:49 +0900

    [level 2] Title: 피보나치 수, Time: 2.90 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int[] dp;
+    
+    public int solution(int n) {
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
