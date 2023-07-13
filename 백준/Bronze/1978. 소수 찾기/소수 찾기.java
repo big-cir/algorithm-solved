e975058a2cb95d15e1a6007e26ebad415acb0e24
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-07-14 02:24:35 +0900

    [Bronze II] Title: 소수 찾기, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] parl = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            parl[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // answer = 소수의 개수
+        int answer = 0;
+
+        for (int x : parl) {
+            if (x == 1) continue;
+            boolean ch = true;
+            for (int i = 2; i < x; i++) {
+                if (x % i == 0) ch = false;
+            }
+
+            if (ch) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
