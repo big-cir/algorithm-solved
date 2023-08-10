78635fda96f06ed07290f0ce9d3872ec4d27caef
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-08-10 23:55:40 +0900

    [Gold III] Title: 세 용액, Time: 236 ms, Memory: 19080 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -2,40 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+   
+    public static long chVal(long sum) {
+        return Math.abs(sum);
+    }
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        int N = Integer.parseInt(br.readLine());
+        long[] answer = new long[3];
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        long min = Long.MAX_VALUE;
 		
-		Arrays.sort(arr);
-		long min = Long.MAX_VALUE;
-		int r1=0, r2=0, r3=0;
-		out : for(int i=0; i<n-2; i++) {
-			int left = i+1;
-			int right = n-1;
-			
-			while(left<right) {
-				long sum = (long)arr[i]+ arr[left] +arr[right];
-				if(min > Math.abs(sum)) {
-					min = Math.abs(sum);
-					r1 =i; r2=left; r3=right;
-				}
-				if(sum==0) {
-					r1 =i; r2=left; r3=right;
-					break out;
-				}else if(sum > 0){
-					right--;
-				}else {
-					left++;
-				}
-				
-			}
-		}
-		System.out.println(arr[r1]+" " + arr[r2] + " " + arr[r3]);
+        for (int i = 0; i < N - 2; i++) {
+            int lt = i + 1;
+            int rt = N - 1;
+            
+            while (lt < rt) {
+                long sum = (long)arr[i] + arr[lt] + arr[rt];
+                long tmp = chVal(sum);
+                if (tmp < min) {
+                    min = tmp;
+                    answer[0] = arr[i];
+                    answer[1] = arr[lt];
+                    answer[2] = arr[rt];
+                }
+                if (sum > 0) {
+                    rt--;
+                } else {
+                    lt++;
+                }
+            }
+        }
+        
+		for (long x : answer) {
+            System.out.print(x + " ");
+        }
 	}
 }
