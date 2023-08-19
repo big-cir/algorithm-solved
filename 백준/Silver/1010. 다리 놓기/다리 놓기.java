91813c3c58df56b05ef33e4587c5c536542b4915
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-08-19 23:31:56 +0900

    [Silver V] Title: 다리 놓기, Time: 164 ms, Memory: 15456 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,28 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static long[][] dp;
 
-    public static int memoization(int N, int M, int[][] dp) {
-        if (dp[N][M] > 0) return dp[N][M];
-        if (N == M || M == 0) {
-            return 1;
-        } else {
-            return dp[N][M] = memoization(N - 1, M - 1, dp) + memoization(N - 1, M, dp);
+    public static void solution(int M, int N) {
+        for (int i = 2; i <= M; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
+            dp = new long[M + 1][N + 1];
+            dp[1][0] = 1;
 
-            int[][] dp = new int[M + 1][N + 1];
-            System.out.println(memoization(M, N, dp));
+            for (int j = 0; j <= M; j++) {
+                dp[j][0] = 1;
+                dp[j][1] = 1;
+            }
+            
+            solution(M, N);
+            System.out.println(dp[M][N]);
         }
     }
 }
