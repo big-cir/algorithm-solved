f3a6b9ab4df941940f7f298398b521cf515802c7
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-09-30 23:24:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 1.55 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,20 +1,34 @@
 class Solution {
     public int solution(int num) {
-        int tmp = num;
-        if (tmp == 1) return 0;
-        
-        int answer = 0;
-        
-        while (tmp != 1) {
-            if (tmp % 2 == 0) {
-                tmp /= 2;
-            } else if (tmp % 2 == 1) {
-                tmp = tmp * 3 + 1;
+        int answer = 0;    
+        while (num != 1) {
+            System.out.println(num);
+            if (isEven(num)) {
+                num /= 2;
+                answer++;
+                continue;
+            }
+            if (isOdd(num)) {
+                num = num * 3 + 1;
             }
             answer++;
-            if (answer > 500) return -1;
+            
+            if (answer > 500) {
+                answer = -1;
+                break;
+            }
         }
         
         return answer;
+    }
+    
+    private boolean isEven(int number) {
+        if (number % 2 == 0) return true;
+        return false;
+    }
+    
+    private boolean isOdd(int number) {
+        if (number % 2 == 1) return true;
+        return false;
     }
 }
