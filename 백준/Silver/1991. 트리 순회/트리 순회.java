b5d3d4a78b0106d4273b5aaf735d3b218df1f1c4
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-10-10 22:55:31 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,83 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
-class Node{
-    char left, right;
-    public Node(char left, char right){
-        this.left = left;
-        this.right = right;
-    }
-}
+import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
 
-    public static List<Node>[] list;
-    public static StringBuilder sb = new StringBuilder();
+    static class Node {
+        private char lt, rt;
 
+        public Node(char lt, char rt) {
+            this.lt = lt;
+            this.rt = rt;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        list = new List[N+1];
-
-        for(int i=1; i<=N; ++i){
-            list[i] = new ArrayList<>();
-        }
+        Map<Character, Node> map = new HashMap<>();
 
         StringTokenizer st;
-        for(int i=1; i<=N; ++i){
+        // . == 46
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int data = st.nextToken().charAt(0) - '@'; // 'A' - '@' == 'A' - 'A' + 1 == 1
-            char l = st.nextToken().charAt(0);
-            char r = st.nextToken().charAt(0);
-            list[data].add(new Node(l, r));
+            char data = st.nextToken().charAt(0);
+            char lt = st.nextToken().charAt(0);
+            char rt = st.nextToken().charAt(0);
+            map.put(data, new Node(lt, rt));
         }
 
-        preorder(1);
+        preOrder(map, 'A');
         sb.append("\n");
-        inorder(1);
+        inOrder(map, 'A');
         sb.append("\n");
-        postorder(1);
-        sb.append("\n");
-        System.out.println(sb);
-
+        postOrder(map, 'A');
+        System.out.println(sb.toString());
     }
 
-    static void preorder(int start){
-        for(Node node: list[start]){
-            char l = node.left;
-            char r = node.right;
-
-            sb.append((char)(start+'@'));
-            if(l != '.') preorder(l-'@');
-            if(r != '.') preorder(r-'@');
-        }
+    public static void preOrder(Map<Character, Node> map, Character parent) {
+        if (parent == '.') return;
+        sb.append(parent);
+        Node childNode = map.get(parent);
+        preOrder(map, childNode.lt);
+        preOrder(map, childNode.rt);
     }
 
-    static void inorder(int start) {
-        for(Node node : list[start]) {
-            char l = node.left;
-            char r = node.right;
-
-            if(l != '.') inorder(l-'@');
-            sb.append((char)(start+'@'));
-            if(r != '.') inorder(r-'@');
-        }
+    public static void inOrder(Map<Character, Node> map, Character parent) {
+        if (parent == '.') return;
+        Node childNode = map.get(parent);
+        inOrder(map, childNode.lt);
+        sb.append(parent);
+        inOrder(map, childNode.rt);
     }
 
-    static void postorder(int start) {
-        for(Node node : list[start]) {
-            char l = node.left;
-            char r = node.right;
-
-            if(l != '.') postorder(l-'@');
-            if(r != '.') postorder(r-'@');
-            sb.append((char)(start+'@'));
-        }
+    public static void postOrder(Map<Character, Node> map, Character parent) {
+        if (parent == '.') return;
+        Node childNode = map.get(parent);
+        postOrder(map, childNode.lt);
+        postOrder(map, childNode.rt);
+        sb.append(parent);
     }
 }
+
