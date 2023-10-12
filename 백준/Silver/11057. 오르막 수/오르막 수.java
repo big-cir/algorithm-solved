35260b0d5f890b21d9f5b855cfb9dd65f16051c6
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-10-12 22:57:41 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int div = 10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[1001][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i < 1001; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                }
+                dp[i][j] %= div;
+            }
+        }
+
+        long answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += dp[N][i];
+        }
+        System.out.println(answer % div);
+    }
+}
