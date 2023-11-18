a655b48bf94a57741f46d959e18943928c78deb8
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-18 19:55:21 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 242 ms, Memory: 41,256 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,82 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Solution {
-	public static int[][] arr = new int[100][100];
-	
-	public static int rowMax() {
-		int max = 0;
-		for (int i = 0; i < 100; i++) {
-			int sum = 0;
-			for (int j = 0; j < 100; j++) {
-				sum += arr[i][j];
-			}
-			
-			max = Math.max(max, sum);
-		}
-		
-		return max;
-	}
-	
-	public static int colMax() {
-		int max = 0;
-		
-		for (int i = 0; i < 100; i++) {
-			int sum = 0;
-			for (int j = 0; j < 100; j++) {
-				sum += arr[j][i];
-			}
-			
-			max = Math.max(max, sum);
-		}
-		
-		return max;
-	}
-	
-	public static int cross1Max() {
-		int max = 0;
-		for (int i = 0; i < 100; i++) {
-			int sum = 0;
-			for (int j = 0; j < 100; j++) {
-				if (i == j) {
-					sum += arr[i][j];
-				} 
-			}
-			
-			max = Math.max(max, sum);
-		}
-		return max;
-	}
-	
-	public static int cross2Max() {
-		int max = 0;
-		for (int i = 99; i >= 0; i--) {
-			int sum = 0;
-			for (int j = 99; j >= 0; j--) {
-				if (i == j) {
-					sum += arr[i][j];
-				} 
-			}
-			
-			max = Math.max(max, sum);
-		}
-		return max;
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 1; i <= 10; i++) {
-			int num = sc.nextInt();
-			for (int j = 0; j < 100; j++) {
-				for (int k = 0; k < 100; k++) {
-					arr[j][k]= sc.nextInt(); 
-				}
-			}
-			
-			int[] maxArr = new int[] {rowMax(), colMax(), cross1Max(), cross2Max()};
-			int max = Arrays.stream(maxArr).max().getAsInt();
-			System.out.println("#" + num + " " + max);	
-		}			
-	}
+    static int[][] map = new int[100][100];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int tc = 1; tc <= 10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int answer = 0;
+            for (int i  = 0; i < 100; i++) {
+                map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            }
+            answer =
+                    Math.max(answer, Math.max(getMaxRow(), Math.max(getMaxColumn(), getMaxDiagonal())));
+            System.out.println("#" + N +  " " + answer);
+        }
+    }
+
+    private static int getMaxRow() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum = Math.max(sum, Arrays.stream(map[i]).sum());
+        }
+
+        return sum;
+    }
+
+    private static int getMaxColumn() {
+        int sum = 0;
+        for (int col = 0; col < 100; col++) {
+            int tmp = 0;
+            for (int row = 0; row < 100; row++) {
+                tmp += map[row][col];
+            }
+            sum = Math.max(sum, tmp);
+        }
+        return sum;
+    }
+
+    private static int getMaxDiagonal() {
+        int diagonal1 = 0;
+        for (int i = 0; i < 100; i++) {
+            diagonal1 += map[i][i];
+        }
+
+        int diagonal2 = 0;
+        for (int i = 0; i < 100; i++) {
+            diagonal2 = map[i][99 - i];
+        }
+
+        return Math.max(diagonal1, diagonal2);
+    }
 }
