f91ff42f98fafe812aeec2f276edb8456a6e5b44
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-18 23:28:35 +0900

    [D3] Title: 농작물 수확하기, Time: 243 ms, Memory: 37,956 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -3,41 +3,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+// 중간 값 N / 2
+// 중간 위
+// 중간 아래
 public class Solution {
-	public static int[][] farm;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int i = 1; i <= T; i++) {
-			int N = Integer.parseInt(br.readLine());
-			farm = new int[N][N];
-			
-			for (int j = 0; j < N; j++) {
-				String[] str = br.readLine().split("");
-				for (int k = 0; k < N; k++) {
-					farm[j][k] = Integer.parseInt(str[k]);
-				}
-			}
+    static int[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseCount = Integer.parseInt(br.readLine());
 
-			int sum = 0;
-			// 중앙 까지 상단부
-			for (int j = 0; j <= (N / 2); j++) {
-				for (int k = (N / 2) - j; k <= (N / 2) + j; k++) {
-					sum += farm[j][k];
-				}
-			}
-			
-			// 중앙(미포함) 부터 하단부
-			int a = 0;
-			for (int j = N - 1; j > (N / 2); j--) {
-				for (int k = (N / 2) - a; k <= (N / 2) + a; k++) {
-					sum += farm[j][k];
-				}
-				a++;
-			}
-			System.out.println("#" + i + " " + sum);
-		}
-	}
+        for (int tc = 1; tc <= caseCount; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                map[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+            }
+            
+            int answer = middleOfUpper(N) + middle(N) + middleOfUnder(N);
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
+
+
+    private static int middleOfUpper(int N) {
+        int size = N / 2;
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            int start = size - i;
+            int end = size + i;
+            for (int j = start; j <= end; j++) {
+                sum += map[i][j];
+            }
+        }
+        return sum;
+    }
+
+    private static int middle(int N) {
+        int[] middleSum = map[N / 2];
+        return Arrays.stream(middleSum).sum();
+    }
+
+    private static int middleOfUnder(int N) {
+        int size = N / 2;
+        int sum = 0;
+        int tmp = 0;
+        for (int i = N - 1; i > size; i--) {
+            int start = size - tmp;
+            int end = size + tmp;
+            for (int j = start; j <= end; j++) {
+                sum += map[i][j];
+            }
+            tmp++;
+        }
+        return sum;
+    }
 }
