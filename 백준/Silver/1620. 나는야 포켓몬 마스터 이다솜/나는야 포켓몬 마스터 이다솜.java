294e92325b45f86598bdf8ee6bc420c80931f7f6
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-21 21:14:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1028 ms, Memory: 168648 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -6,28 +6,35 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());       // 포켓몬 개수
-        int M = Integer.parseInt(st.nextToken());       // 맞춰야하는 문제 개수
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
 
-        Map<Integer, String> map = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        int value = 1;
+        Map<String, Integer> dict = new HashMap<>();
+        Map<Integer, String> reverseIdx = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            map.put(i + 1, str);
-            map2.put(str, i + 1);
+            String input = br.readLine();
+            dict.put(input, value++);
+        }
+
+        for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+            reverseIdx.put(entry.getValue(), entry.getKey());
         }
 
         for (int i = 0; i < M; i++) {
             String input = br.readLine();
             if (input.matches("^[0-9]*$")) {
-                int value = Integer.parseInt(input);
-                System.out.println(map.get(value));
+                sb.append(reverseIdx.get(Integer.parseInt(input)) + "\n");
             } else {
-                System.out.println(map2.get(input));
+                sb.append(dict.get(input) + "\n");
             }
         }
+        System.out.println(sb);
     }
 }
