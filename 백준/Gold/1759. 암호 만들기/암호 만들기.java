4aa5a9539525979c42a7971254c15caee87b23a5
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-23 22:53:04 +0900

    [Gold V] Title: 암호 만들기, Time: 3412 ms, Memory: 92788 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -7,7 +7,7 @@ public class Main {
 
     static int L, C;
     static Set<Character> consonant = new HashSet<>(List.of('a', 'e', 'i', 'o', 'u'));
-    static char[] alpa;
+    static char[] alpa, result;
     static int[] ch;
 
     public static void main(String[] args) throws IOException {
@@ -16,6 +16,7 @@ public class Main {
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         ch = new int[C];
+        result = new char[L];
         alpa = new char[C];
 
         st = new StringTokenizer(br.readLine());
@@ -24,15 +25,14 @@ public class Main {
         }
 
         Arrays.sort(alpa);
-        sol(0, new StringBuilder(), 0);
+        sol(0, 0);
     }
 
-    private static void sol(int Level, StringBuilder sb, int idx) {
+    private static void sol(int Level, int idx) {
         if (Level == L) {
             int conso = 0;
             int vowel = 0;
-            char[] chars = sb.toString().toCharArray();
-            for (char x : chars) {
+            for (char x : result) {
                 if (consonant.contains(x)) {
                     conso++;
                     continue;
@@ -41,8 +41,8 @@ public class Main {
             }
 
             if (conso >= 1 && vowel >= 2) {
-                if (isSorted(chars)) {
-                    System.out.println(sb);
+                if (isSorted(result)) {
+                    System.out.println(new String(result));
                 }
             }
             return;
@@ -51,20 +51,16 @@ public class Main {
         for (int i = idx; i < C; i++) {
             if (ch[i] == 0) {
                 ch[i] = 1;
-                sb.append(alpa[i]);
-                sol(Level + 1, sb, idx + 1);
-                sb.setLength(sb.length() - 1);
+                result[Level] = alpa[i];
+                sol(Level + 1, idx + 1);
                 ch[i] = 0;
             }
         }
     }
 
     private static boolean isSorted(char[] chars) {
-        String tmp = new String(chars);
-        Arrays.sort(chars);
-
-        String comp = new String(chars);
-        if (tmp.equals(comp)) return true;
-        return false;
+        char[] sortedChars = Arrays.copyOf(chars, chars.length);
+        Arrays.sort(sortedChars);
+        return Arrays.equals(chars, sortedChars);
     }
 }
