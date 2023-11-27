0c119d3b63ddf32a40809c420c195b1d1db65fac
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-27 12:47:08 +0900

    [Silver V] Title: 단어 정렬, Time: 576 ms, Memory: 23656 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            list.add(word);
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(list.get(0));
+        for (int i = 1; i < list.size(); i++) {
+            if (!list.get(i).equals(list.get(i - 1))) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
