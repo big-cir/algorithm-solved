ad365464eeb60d750a9811db2222f40c4e53c91b
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-11-30 13:52:14 +0900

    [Silver IV] Title: 듣보잡, Time: 480 ms, Memory: 23884 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,29 +4,50 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M;
 
+    static int N, M;
+    static String[] heard;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        binarySearch(br);
+    }
 
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            set.add(br.readLine());
-        }
-
+    private static void binarySearch(BufferedReader br) throws IOException {
         List<String> answer = new ArrayList<>();
+        heard = new String[N];
+        for (int i = 0; i < N; i++) {
+            heard[i] = br.readLine();
+        }
+        Arrays.sort(heard);
+
+        int count = 0;
         for (int i = 0; i < M; i++) {
-            String compare = br.readLine();
-            if (set.contains(compare)) {
-                answer.add(compare);
+            String target = br.readLine();
+
+            int lt = 0;
+            int rt = N - 1;
+            while (lt <= rt) {
+                int mid = (lt + rt) / 2;
+
+                if (heard[mid].equals(target)) {
+                    count++;
+                    answer.add(target);
+                    break;
+                }
+
+                if (heard[mid].compareTo(target) < 0) {
+                    lt = mid + 1;
+                } else {
+                    rt = mid - 1;
+                }
             }
         }
 
         Collections.sort(answer);
-        System.out.println(answer.size());
+        System.out.println(count);
         for (String x : answer) {
             System.out.println(x);
         }
