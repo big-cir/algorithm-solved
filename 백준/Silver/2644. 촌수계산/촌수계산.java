4c0e6c407e71ed53b7a46b962b642cfbb226f8d5
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-12-22 22:21:29 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,26 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, num1, num2;
-    static int[] ch, answer;
+
+    static int N, M, to, from, answer;
     static List<List<Integer>> graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        num1 = Integer.parseInt(st.nextToken());
-        num2 = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(br.readLine());
-
         graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
-        for (int i = 1; i <= M; i++) {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        to = Integer.parseInt(st.nextToken());
+        from = Integer.parseInt(st.nextToken());
+
+        M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
@@ -28,29 +30,23 @@ public class Main {
             graph.get(v2).add(v1);
         }
 
-        ch = new int[N + 1];
-        answer = new int[N + 1];
-        bfs();
-        System.out.println(answer[num2]);
+        int[] ch = new int[N + 1];
+        answer = -1;
+        dfs(to, from, ch, 0);
+        System.out.println(answer);
     }
 
-    public static void bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(num1);
-        ch[num1] = 1;
+    private static void dfs(int to, int from, int[] ch, int count) {
+        if (to == from) {
+            answer = count;
+            return;
+        }
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int x : graph.get(cur)) {
-                if (ch[x] == 0) {
-                    ch[x] = 1;
-                    answer[x] += answer[cur] + 1;
-                    if (x == num2) return;
-                    queue.offer(x);
-                }
+        ch[to] = 1;
+        for (int nextV : graph.get(to)) {
+            if (ch[nextV] == 0) {
+                dfs(nextV, from, ch, count + 1);
             }
         }
-       
-        if (answer[num2] == 0) answer[num2] = -1;
     }
 }
