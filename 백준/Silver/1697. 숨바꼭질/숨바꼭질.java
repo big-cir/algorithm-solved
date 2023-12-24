202376be48365e55fa1651a9ec6d28984849c6d2
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2023-12-24 21:41:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,53 +6,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K, answer = 0, size = 100001;
-    static int[] dis;
+    static int ran = 100001;
+    static int N, K, answer;
+    static int[] dis = new int[ran];
 
     public static void BFS(int N) {
-        dis[N] = 1;
+        dis[N] = 1;                 // 수빈이의 위치
         Queue<Integer> queue = new LinkedList<>();
         queue.add(N);
 
         while (!queue.isEmpty()) {
-            int curIdx = queue.poll();
-            if (curIdx == K) {
-                answer = dis[curIdx] - 1;
+            // 현재 인덱스
+            int cur = queue.poll();
+
+            // index 가 17이면 동생에게 도착. 시간 출력
+            if (cur == K) {
+                answer = dis[cur] - 1;
                 return;
             }
 
-            if (curIdx == 0) {
-                if (dis[curIdx + 1] == 0) {
-                    dis[curIdx + 1] = dis[curIdx] + 1; // 걷기
-                    dis[curIdx * 2] = dis[curIdx] + 1;
-                    queue.offer(curIdx + 1);
+            if (cur == 0) {
+                if (dis[cur + 1] == 0) {
+                    dis[cur + 1] = dis[cur] + 1;
+                    dis[cur * 2] = dis[cur] + 1;
+                    queue.offer(cur + 1);
                 }
             }
 
-            if (curIdx > 0 && dis[curIdx - 1] == 0) {
-                dis[curIdx - 1] = dis[curIdx] + 1;
-                queue.offer(curIdx - 1);
+            // 걷기 X - 1
+            if (cur > 0 && dis[cur - 1] == 0) {
+                dis[cur - 1] = dis[cur] + 1;
+                queue.offer(cur - 1);
             }
 
-            if (curIdx < size - 1 && dis[curIdx + 1] == 0) {
-                dis[curIdx + 1] = dis[curIdx] + 1;
-                queue.offer(curIdx + 1);
+            // 걷기 X + 1
+            if (cur < ran - 1 && dis[cur + 1] == 0) {
+                dis[cur + 1] = dis[cur] + 1;
+                queue.offer(cur + 1);
             }
 
-            if (curIdx * 2 < size && dis[curIdx * 2] == 0) {
-                dis[curIdx * 2] = dis[curIdx] + 1;
-                queue.offer(curIdx * 2);
+            // 순간이동 X * 2
+            if (cur * 2 < ran && dis[cur * 2] == 0  ) {
+                dis[cur * 2] = dis[cur] + 1;
+                queue.offer(cur * 2);
             }
+
+
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        dis = new int[size];
-        BFS(N);
 
+        BFS(N);
         System.out.println(answer);
     }
 }
