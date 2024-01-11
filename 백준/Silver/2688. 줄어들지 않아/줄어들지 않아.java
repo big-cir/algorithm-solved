272b435093ca46e68d3eadd9cdf274684098351a
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-01-11 22:35:29 +0900

    [Silver I] Title: 줄어들지 않아, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= tc; i++) {
+            int n = Integer.parseInt(br.readLine());
+            long[][] dp = new long[n + 1][10];
+
+            // 초기화
+            for (int j = 0; j <= 9; j++) {
+                dp[1][j] = 1;
+            }
+
+            for (int j = 2; j <= n; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    for (int l = k; l <= 9; l++) {
+                        dp[j][k] += dp[j - 1][l];
+                    }
+                }
+            }
+
+
+            long answer = 0;
+            for (int j = 0; j <= 9; j++) {
+                answer += dp[n][j];
+            }
+            System.out.println(answer);
+        }
+    }
+}
