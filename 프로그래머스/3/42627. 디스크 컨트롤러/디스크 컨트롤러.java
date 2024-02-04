1ac61ae5336203b1a55d1953eb98dd1c3f91cf1f
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-04 19:22:19 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.59 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,54 +1,54 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Queue;
 import java.util.PriorityQueue;
 
 class Solution {
-    static class Node implements Comparable<Node> {
-        int request;
+    static class Work implements Comparable<Work> {
+        int start;
         int cost;
         
-        Node(int request, int cost) {
-            this.request = request;
+        public Work(int start, int cost) {
+            this.start = start;
             this.cost = cost;
         }
         
         @Override
-        public int compareTo(Node node) {
-            return cost - node.cost;
+        public int compareTo(Work o) {
+            return cost - o.cost;
         }
     }
     
     public int solution(int[][] jobs) {
         int answer = 0;
+        Arrays.sort(jobs, (a1, b1) -> a1[0] - b1[0]);
         
-        Arrays.sort(jobs, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int completedJobs = 0;
         int jobIdx = 0;
         int currentTime = 0;
-
-        while (completedJobs < jobs.length) {
+        int completedJob = 0;
+        Queue<Work> pq = new PriorityQueue<>();
+        
+        while (completedJob < jobs.length) {
+            // jobIdx는 jobs.length 보다 작아야하고,
+            // 요청 시간은 현재 시간보다 작거나 같아야 함.
             while (jobIdx < jobs.length && jobs[jobIdx][0] <= currentTime) {
-                pq.offer(new Node(jobs[jobIdx][0], jobs[jobIdx][1]));
+                pq.offer(new Work(jobs[jobIdx][0], jobs[jobIdx][1]));
                 jobIdx++;
             }
-
+            
             if (!pq.isEmpty()) {
-                Node now = pq.poll();
+                Work now = pq.poll();
                 currentTime += now.cost;
-                answer += (currentTime - now.request);
-                completedJobs++;
+                answer += (currentTime - now.start);
+                completedJob++;
             } else {
+                // currentTime += pq.peek().start;
+                // A작업이 완료되고 현재 시간은 3일 때, B의 작업 시작시간이 5라면
+                // 현재 우선순위 큐에는 작업 목록이 없다.
+                // 두번쨰 while문 조건을 맞추면서 두 번째 작업을 큐에 추가하기 위해서 조건이 동일하도록 시간을 변경해야 함.
                 currentTime = jobs[jobIdx][0];
             }
         }
         
-        return answer / completedJobs;
+        return answer / jobs.length;
     }
 }
