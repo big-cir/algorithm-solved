dc8692fc9a62fd3732988f854a9e5ca8e3d83e55
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-06 23:38:30 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,31 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int[] nums;
-    static int count;
-    public static void DFS(int n, int num) {
-        if (n == num) {
-            count++;
-            return;
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                int sum = n + nums[i];
-                if (sum > num) {
-                    continue;
-                }
-                DFS(sum, num);
-            }
-        }
-    }
+    static int tc;
+    static int[] dp = new int[12];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        nums = new int[] {1, 2, 3};
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            count = 0;
-            DFS(0, sc.nextInt());
-            System.out.println(count);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        tc = Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        dp[4] = 7;
+
+        for (int i = 0; i < tc; i++) {
+            int N = Integer.parseInt(br.readLine());
+            for (int j = 5; j <= N; j++) {
+                dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+            }
+            System.out.println(dp[N]);
         }
     }
 }
