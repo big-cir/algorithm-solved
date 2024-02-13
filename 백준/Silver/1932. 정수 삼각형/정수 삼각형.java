ebc8aef57caf3466e151e9ab1b55cbe420638961
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-13 20:48:14 +0900

    [Silver I] Title: 정수 삼각형, Time: 320 ms, Memory: 29804 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -13,15 +13,12 @@ public class Main {
 
         dp[0][0] = arr[0][0];
 
+        for (int i = 1 ; i < N; i++) {
+            dp[i][0] = dp[i - 1][0] + arr[i][0]; 
+        }
         for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                if (j == N - 1) {
-                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
-                } else if (j == 0) {
-                    dp[i][0] = dp[i - 1][0] + arr[i][0];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
-                }
+            for (int j = 1; j < i + 1; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
             }
         }
         
