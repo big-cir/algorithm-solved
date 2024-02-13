df695fd69869c8a03164de3e33434f3ddc1069ad
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-13 20:45:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 344 ms, Memory: 31644 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < N; i++) arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[][] dp = new int[N][N];
+
+        dp[0][0] = arr[0][0];
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == N - 1) {
+                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+                } else if (j == 0) {
+                    dp[i][0] = dp[i - 1][0] + arr[i][0];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[N - 1][i]);
+        }
+        System.out.println(answer);
+    }
+}
