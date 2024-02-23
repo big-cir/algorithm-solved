8b8e9943285e31990ec3111c28af28fe1795b545
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-23 12:16:58 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,8 @@
+# select * from ANIMAL_OUTS
+# order by DATETIME
+
+select hour(DATETIME) as HOUR, count(ANIMAL_ID) as COUNT
+from ANIMAL_OUTS
+where hour(DATETIME) between 9 and 19
+group by HOUR
+order by HOUR
