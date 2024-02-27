594d106867184a87fe06e66c114e3a7e2f515dd8
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-02-27 22:03:43 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+select BOARD_ID, WRITER_ID, TITLE, PRICE,
+    case
+    when STATUS = 'DONE' then '거래완료'
+    when STATUS = 'SALE' then '판매중'
+    else '예약중'
+    end as STATUS
+from USED_GOODS_BOARD
+where date_format(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
+order by BOARD_ID desc
