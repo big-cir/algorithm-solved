269d7dc5a7ffa76e8ac4a5194b48edb7c392f3e1
Dae Won <99483390+Big-Cir97@users.noreply.github.com>
2024-04-02 00:07:52 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 66.12 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,40 +1,39 @@
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
+    // 길이 <= 300,000;
     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
+        int answer = Integer.MAX_VALUE;
         Queue<Integer> q1 = new LinkedList<>();
         for (int x : queue1) q1.offer(x);
         Queue<Integer> q2 = new LinkedList<>();
-        for (int x :queue2) q2.offer(x);
-
+        for (int x : queue2) q2.offer(x);
+        
         long sum1 = Arrays.stream(queue1).sum();
         long sum2 = Arrays.stream(queue2).sum();
-
-        answer = 0;
-        while (sum1 != sum2) {
-            if (answer > (queue1.length + queue2.length) * 2) {
-                answer = -1;
-                break;
+        int maxChance = (queue1.length + queue2.length) * 2;
+        
+        int cnt = 0;
+        while (sum1 != sum2 && cnt < maxChance) {
+            if (sum1 == sum2) break;
+            else if (sum1 < sum2) {
+                int tmp = q2.poll();
+                q1.offer(tmp);
+                sum1 += tmp;
+                sum2 -= tmp;
+            } else {
+                int tmp = q1.poll();
+                q2.offer(tmp);
+                sum2 += tmp;
+                sum1 -= tmp;
             }
-            
-            if (sum1 < sum2) {
-                int val2 = q2.poll();
-                q1.offer(val2);
-
-                sum1 += val2;
-                sum2 -= val2;
-            } else if (sum1 > sum2) {
-                int val1 = q1.poll();
-                q2.offer(val1);
-
-                sum1 -= val1;
-                sum2 += val1;
-            }
-            answer++;
+            cnt++;
         }
+        answer = Math.min(answer, cnt);
+        
+        if (answer == maxChance) return -1;
         return answer;
     }
 }
