00176ee946931f862e45a9a23b43132e12bfe7ac
dae won <99483390+big-cir@users.noreply.github.com>
2024-04-19 22:44:29 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17404 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -5,35 +5,43 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int K, N;
+    static int[] lines;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        int[] A = new int[K];
+        lines = new int[K];
         for (int i = 0; i < K; i++) {
-            A[i] = Integer.parseInt(br.readLine());
+            lines[i] = Integer.parseInt(br.readLine());
         }
 
-        long lt = 1;
-        long rt = Arrays.stream(A).max().getAsInt();
+        long max = Long.MIN_VALUE;
+        long lt = 0;
+        long rt = Long.MAX_VALUE;
 
         while (lt <= rt) {
-            int count = 0;
             long mid = (lt + rt) / 2;
 
-            for (int i = 0; i < K; i++) {
-                count += (A[i] / mid);
-            }
-
-            if (count < N) {
-                rt = mid - 1;
-            } else {
+            if (N <= check(mid)) {
+                max = mid;
                 lt = mid + 1;
+            } else {
+                rt  = mid - 1;
             }
         }
+        System.out.println(max);
+    }
 
-        System.out.println(rt);
+    private static int check(long mid) {
+        int cnt = 0;
+        for (int i = 0; i < K; i++) {
+            cnt += (lines[i] / mid);
+        }
+
+        return cnt;
     }
 }
