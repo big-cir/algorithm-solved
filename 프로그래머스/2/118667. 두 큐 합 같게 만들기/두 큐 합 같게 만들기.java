c1bbc065427363e7add6f3b8bd7cb99510ea3182
dae won <99483390+big-cir@users.noreply.github.com>
2024-04-24 09:42:34 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 51.26 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,39 +1,35 @@
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    // 길이 <= 300,000;
     public int solution(int[] queue1, int[] queue2) {
-        int answer = Integer.MAX_VALUE;
         Queue<Integer> q1 = new LinkedList<>();
         for (int x : queue1) q1.offer(x);
+        
         Queue<Integer> q2 = new LinkedList<>();
         for (int x : queue2) q2.offer(x);
         
+        int answer = 0;
+        int size = (queue1.length + queue2.length) * 2;
+        
         long sum1 = Arrays.stream(queue1).sum();
         long sum2 = Arrays.stream(queue2).sum();
-        int maxChance = (queue1.length + queue2.length) * 2;
-        
-        int cnt = 0;
-        while (sum1 != sum2 && cnt < maxChance) {
+        while (answer <= size) {
             if (sum1 == sum2) break;
-            else if (sum1 < sum2) {
-                int tmp = q2.poll();
-                q1.offer(tmp);
-                sum1 += tmp;
-                sum2 -= tmp;
+            
+            if (sum1 < sum2) {
+                int val = q2.poll();
+                sum1 += val;
+                sum2 -= val;
+                q1.offer(val);
             } else {
-                int tmp = q1.poll();
-                q2.offer(tmp);
-                sum2 += tmp;
-                sum1 -= tmp;
+                int val = q1.poll();
+                sum1 -= val;
+                sum2 += val;
+                q2.offer(val);
             }
-            cnt++;
+            answer++;
         }
-        answer = Math.min(answer, cnt);
         
-        if (answer == maxChance) return -1;
-        return answer;
+        return answer == size + 1 ? -1 : answer;
     }
 }
