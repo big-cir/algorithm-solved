bfd2aa456169b504997ba165a91c32b9a562af23
dae won <99483390+big-cir@users.noreply.github.com>
2024-05-01 10:25:37 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.12 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,83 +1,72 @@
 class Solution {
-    // 1. Lock 확장
-    // 2. 확장된 배열에서 key를 회전하면서 탐색
-    // 3. key를 맞춘 부분 +1, 끝나면 다시 -1
-    // 4. lock 범위가 전부 1이면 true
+    static int n, m, size;
+    static int[][] map;
+    
     public boolean solution(int[][] key, int[][] lock) {
-        int nKey = key.length;
-        int nLock = lock.length;
-        
-        // 1.
-        int extendSize = 2 * (nKey - 1) + nLock;
-        int[][] extendLock = new int[extendSize][extendSize];
-        for (int i = nKey - 1; i < extendSize - (nKey - 1); i++) {
-            for (int j = nKey - 1; j < extendSize - (nKey - 1); j++) {
-                int row = i - (nKey - 1);
-                int col = j - (nKey - 1);
-                extendLock[i][j] = lock[row][col];
+        n = key.length;
+        m = lock.length;
+
+        // 1. 초기화
+        size = m + 2 * (n - 1);
+        map = new int[size][size];
+        for (int i = n - 1; i < size - (n - 1); i++) {
+            for (int j = n - 1; j < size - (n - 1); j++) {
+                map[i][j] = lock[i - (n - 1)][j - (n - 1)];
             }
-        }
-        
-        // 2.
-        for (int i = 0; i < extendSize - nKey + 1; i++) {
-            for (int j = 0; j < extendSize - nKey + 1; j++) {
-                int[][] copy = new int[nKey][nKey];
-                for (int k = 0; k < nKey; k++) copy[k] = key[k].clone();
-                if (isMatch(i, j, nKey, extendSize, extendLock, copy)) return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isMatch(int startX, int startY, int nKey, int extendSize, int[][] extendLock, int[][] copy) {
-        for (int i = 0; i < 4; i++) {
-            rotation(nKey, copy);
-            
-            for (int j = 0; j < nKey; j++) {
-                for (int k = 0; k < nKey; k++) {
-                    extendLock[startX + j][startY + k] += copy[j][k];
-                }
-            }
-            
-            // 키가 맞는지 확인하는 작업
-            boolean check = true;
-            for (int j = nKey - 1; j < extendSize - nKey + 1; j++) {
-                for (int k = nKey - 1; k < extendSize - nKey + 1; k++) {
-                    if (extendLock[j][k] != 1) {
-                        check = false;
-                        break;
-                    }
-                }
-            }
-            
-            if (check) return true;
-            
-            // 3.
-            recovery(startX, startY, nKey, extendLock, copy);
-        }
-        return false;
-    }
-    
-    // extendLock 원상 복구
-    private void recovery(int startX, int startY, int nKey, int[][] extendLock, int[][] copy) {
-        for (int i = 0; i < nKey; i++) {
-            for (int j = 0; j < nKey; j++) {
-                extendLock[startX + i][startY + j] -= copy[i][j];
-            }
-        }
-    }
-    
-    private void rotation(int nKey, int[][] copy) {
-        int[][] tmp = new int[nKey][nKey];
-        for (int i = 0; i < nKey; i++) {
-            tmp[i] = copy[i].clone();
         }
 
-        for (int i = 0; i < nKey; i++) {
-            for (int j = 0; j < nKey; j++) {
-                copy[i][j] = tmp[j][nKey - i - 1];
+        // 2. (0, 0)부터 key 맞추기
+        // (n-1) -> (n + 1)까지
+        for (int i = 0; i < size - (n - 1); i++) {
+            for (int j = 0; j < size - (n - 1); j++) {
+                // 2-1. key를 90도 돌리면서 lock에 match 되는지 확인
+                for (int k = 0; k < 4; k++) {
+                    key = rotation(key);
+                    if (isMatches(i, j, key)) return true;
+                }
             }
         }
+
+        return false;
+    }
+
+    private boolean isMatches(int x, int y, int[][] key) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int row = x + i;
+                int col = y + j;
+                map[row][col] += key[i][j];
+            }
+        }
+
+        for (int i = n - 1; i < map.length - (n - 1); i++) {
+            for (int j = n - 1; j < map.length - (n - 1); j++) {
+                if (map[i][j] != 1) {
+                    // 원상태로
+                    for (int k = 0; k < n; k++) {
+                        for (int l = 0; l < n; l++) {
+                            int row = x + k;
+                            int col = y + l;
+                            map[row][col] -= key[k][l];
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // 90도 회전
+    private int[][] rotation(int[][] key) {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = key[j][n - i - 1];
+            }
+        }
+
+        return copy;
     }
 }
