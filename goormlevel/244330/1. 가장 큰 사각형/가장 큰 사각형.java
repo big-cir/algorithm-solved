13424264b3f47be270bd64b7fc736c3b8b98963f
dae won <99483390+big-cir@users.noreply.github.com>
2024-06-11 11:33:44 +0900

    [goormlevel] Title: 가장 큰 사각형, Time: 0.10 ms, Memory: 31.34 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/244330/1. 가장 큰 사각형/가장 큰 사각형.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int max = Integer.MIN_VALUE;
+		
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			max = Math.max(max, x * y);
+		}
+		
+		System.out.println(max);
+	}
+}
