4bf2f80fdc2a1b340574598d8a152c1fea75022a
dae won <99483390+big-cir@users.noreply.github.com>
2024-07-31 22:16:42 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 5.44 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,63 +1,63 @@
-
 class Solution {
+    
+    static String answer;
     static int[] dx = {1, 0, 0, -1};
     static int[] dy = {0, -1, 1, 0};
+    static String[] words = {"d", "l", "r", "u"};
     static int[][] map;
-    static boolean flag;
-    static String[] word = {"d", "l", "r", "u"};
-    static String answer;
-    static StringBuilder sb = new StringBuilder();
     
-
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         map = new int[n][m];
-        map[x - 1][y - 1] = 2;
-        map[r - 1][c - 1] = 3;
+        map[x - 1][y - 1] = 2; // start
+        map[r - 1][c - 1] = 3; // end
         
-        answer = "zzzzz";
-        
-        int distance = calculateDis(x - 1, y - 1, r - 1, c - 1);
+        int distance = getDis(x - 1, y - 1, r - 1, c - 1);
         if (distance > k) return "impossible";
         
-        flag = false;
-        dfs(0, k, x - 1, y - 1, r - 1, c - 1);
+        answer = "zzzzz";
+        backtrack(k, x - 1, y - 1, r - 1, c - 1, new StringBuilder());
         
-        return answer.equals("zzzzz") ? "impossible" : answer;
+        if ("zzzzz".equals(answer)) {
+            return "impossible";
+        }
+        
+        return answer;
     }
     
-    private void dfs(int level, int k, int x, int y, int r, int c) {
+    private void backtrack(int k, int x, int y, int r, int c, StringBuilder sb) {
         if (!answer.equals("zzzzz")) return;
-        
-        if ((k - level - calculateDis(x, y, r, c)) % 2!= 0 || k - level < calculateDis(x, y, r, c)) {
-            return;
-        }
         
         if (sb.length() > k) return;
         
-        if (calculateDis(x, y, r, c) > k - sb.length()) return;
+        // 도착까지 거리가 주어진 k 보다 큰지 판단
+        if (k - sb.length() < getDis(x, y, r, c)) return;
         
-        if (sb.length() == k && map[x][y] == 3) {
-            if (sb.toString().compareTo(answer) < 0) {
-                flag = true;
-                answer = sb.toString();
+        // 남은 거리가 홀수인데 현재 위치에서 도착까지 짝수인 경우
+        if ((k - sb.length() - getDis(x, y, r, c)) % 2 != 0) return;
+        
+        if (sb.length() == k) {
+            if (map[x][y] == 3) {
+                if (sb.toString().compareTo(answer) < 0) {
+                    answer = sb.toString();
+                }
             }
             
             return;
         }
-
+        
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
+            
             if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
-                sb.append(word[i]);
-                dfs(level + 1, k, nx, ny, r, c);
+                sb.append(words[i]);
+                backtrack(k, nx, ny, r, c, sb);
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
     }
     
-    private int calculateDis(int x, int y, int r, int c) {
-        return Math.abs(x - r) + Math.abs(y - c);
+    private int getDis(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 }
