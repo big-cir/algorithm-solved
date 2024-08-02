6b4825b3d7243d59cc6d0d40cea65c23c2c13057
dae won <99483390+big-cir@users.noreply.github.com>
2024-08-02 20:40:39 +0900

    정수 n개의 합 2.java

---
+++ tree/정수 n개의 합 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] sum = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+        
+        int answer = 0;
+
+        for (int i = 1; i <= N - K; i++) {
+            answer = Math.max(answer, sum[i + K] - sum[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
