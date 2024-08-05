fc3c44a3d7035f242ac8e603d6b0575708d1dbba
dae won <99483390+big-cir@users.noreply.github.com>
2024-08-05 23:03:43 +0900

    [Gold IV] Title: 겹치는 선분, Time: 1808 ms, Memory: 354960 KB -BaekjoonHub

---
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<int[]> points = new ArrayList<>();
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            points.add(new int[] {start, 1});
+            points.add(new int[] {end, -1});
+        }
+
+        Collections.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        int max = 0;
+        int cnt = 0;
+        for (int i = 0; i < points.size(); i++) {
+            int[] point = points.get(i);
+            int value = point[1];
+
+            if (value == 1) {
+               cnt++;
+               max = Math.max(cnt, max);
+            } else cnt--;
+        }
+
+        System.out.println(max);
+    }
+}
