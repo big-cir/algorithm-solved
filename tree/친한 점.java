69db5995da6f759fe3ba5078eb3b8d38dce5fd3c
dae won <99483390+big-cir@users.noreply.github.com>
2024-08-12 13:39:32 +0900

    친한 점.java

---
+++ tree/친한 점.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        TreeSet<int[]> treeSet = new TreeSet<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            treeSet.add(new int[] {x, y});
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int[] point = new int[] {x, y};
+            int[] higher = treeSet.higher(point);
+
+            if (higher == null) {
+                System.out.println(-1 + " " + -1);
+            } else {
+                System.out.println(higher[0] + " " + higher[1]);
+            }
+        }
+    }
+}
