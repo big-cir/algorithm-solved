f4f20ea088b220f5d7d9155155b868c19331eeee
dae won <99483390+big-cir@users.noreply.github.com>
2024-08-25 00:40:17 +0900

    [Gold III] Title: 소수의 연속합, Time: 1852 ms, Memory: 24560 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -2,37 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    static int answer;
+
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
-        int[] arr = getPrimeNums(target);
+        int N = Integer.parseInt(br.readLine());
+        getPrimeNums(N);
 
         int lt = 0;
         int rt = 0;
         int sum = 0;
         int answer = 0;
+
         while (lt <= rt && rt < arr.length) {
             sum += arr[rt];
-            if (sum <= target) {
-                if (sum == target) answer++;
+            if (sum <= N) {
+                if (sum == N) answer++;
                 rt++;
             } else {
-                sum -= arr[lt++];
                 sum -= arr[rt];
+                sum -= arr[lt++];
             }
         }
+
         System.out.println(answer);
     }
 
-    private static int[] getPrimeNums(int N) {
+    private static void getPrimeNums(int N) {
         List<Integer> list = new ArrayList<>();
         for (int i = 2; i <= N; i++) {
             boolean flag = true;
-
             for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     flag = false;
@@ -43,8 +47,6 @@ public class Main {
             if (flag) list.add(i);
         }
 
-        int[] arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) arr[i] = list.get(i);
-        return arr;
+        arr = list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
