a60fdcb870dee5ca8f013b8045701d546dc2e0e2
dae won <99483390+big-cir@users.noreply.github.com>
2024-09-17 00:37:38 +0900

    [Gold IV] Title: 서강그라운드, Time: 108 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,64 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     static int N, M, R;
-    static int INF = 100 * 15 * 100 + 1;
     static int[] item;
-    static int[][] graph;
+    static List<List<int[]>> graph;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        item = new int[N];
-        graph = new int[N][N];
+        int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = info[0];
+        M = info[1];
+        R = info[2];
 
-        for (int[] arr : graph) {
-            Arrays.fill(arr, INF);
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == j) graph[i][j] = 0;
-            }
-        }
+        item = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            item[i] = Integer.parseInt(st.nextToken());
-        }
-
+        StringTokenizer st;
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken()) - 1;
-            int v2 = Integer.parseInt(st.nextToken()) - 1;
-            int w = Integer.parseInt(st.nextToken());
-            graph[v1][v2] = w;
-            graph[v2][v1] = w;
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph.get(v1).add(new int[] {v2, cost});
+            graph.get(v2).add(new int[] {v1, cost});
         }
 
-        // 예은이 수색범위 M, 길의 갯수
         int answer = 0;
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        for (int i = 1; i <= N; i++) {
+            int[] dis = dijk(i);
+            int tmp = 0;
+            for (int j = 1; j <= N; j++) {
+                if (dis[j] <= M) {
+                    tmp += item[j - 1];
+                }
+            }
+
+            answer = Math.max(tmp, answer);
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int[] dijk(int start) {
+        int[] dis = new int[N + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[start] = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int[] next : graph.get(now)) {
+                if (dis[next[0]] > dis[now] + next[1]) {
+                    dis[next[0]] = dis[now] + next[1];
+
+                    if (dis[next[0]] <= M) {
+                        queue.offer(next[0]);
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            int max = 0;
-            for (int j = 0; j < N; j++) {
-                if (graph[i][j] <= M) max += item[j];
-            }
-            answer = Math.max(answer, max);
-        }
-
-        System.out.println(answer);
+        return dis;
     }
 }
