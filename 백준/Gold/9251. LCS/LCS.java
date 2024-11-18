e0e8ebbf5bc851ad640a1adcaa505e85deb1a373
dae won <99483390+big-cir@users.noreply.github.com>
2024-11-18 20:29:48 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18476 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,28 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str1 = br.readLine();
         String str2 = br.readLine();
+        int size1 = str1.length();
+        int size2 = str2.length();
 
-        char[] ch1 = str1.toCharArray();
-        char[] ch2 = str2.toCharArray();
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-        for (int i = 1; i <= ch1.length; i++) {
-            char str1Ch = ch1[i - 1];
-            for (int j = 1; j <= ch2.length; j++) {
-                char str2Ch = ch2[j - 1];
-                if (str1Ch == str2Ch) {
+        int[][] dp = new int[size1 + 1][size2 + 1];
+        for (int i = 1; i <= size1; i++) {
+            for (int j = 1; j <= size2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(dp[str1.length()][str2.length()]);
+
+        System.out.println(dp[size1][size2]);
     }
 }
