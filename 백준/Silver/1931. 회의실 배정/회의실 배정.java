97b79d7e0c1d871f86f600f5cef1b2f087bb70ed
dae won <99483390+big-cir@users.noreply.github.com>
2024-11-27 18:40:16 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 45284 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,27 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;       // 회의 개수
 
-    public static class Meet implements Comparable<Meet> {
-        private int startTime;
-        private int endTime;
+    static class Office implements Comparable<Office> {
+        int start;
+        int end;
 
-        public Meet(int startTime, int endTime) {
-            this.startTime = startTime;
-            this.endTime = endTime;
+        public Office(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
 
         @Override
-        public int compareTo(Meet o) {
-            if (endTime == o.endTime) return startTime - o.startTime;
-            return endTime - o.endTime;
+        public int compareTo(Office o) {
+            if (this.end == o.end) {
+                return this.start - o.start;
+            }
+
+            return this.end - o.end;
         }
     }
 
@@ -29,22 +26,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        List<Meet> meets = new ArrayList<>();
+        Office[] offices = new Office[N];
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            meets.add(new Meet(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            offices[i] = new Office(start, end);
         }
-        Collections.sort(meets);
-
-        int max = 0;
+        Arrays.sort(offices);
+        
+        int answer = 0;
         int time = 0;
-        for (int i = 0; i < meets.size(); i++) {
-            Meet curMeet = meets.get(i);
-            if (time <= curMeet.startTime) {
-                max++;
-                time = curMeet.endTime;
+        for (int i = 0; i < offices.length; i++) {
+            Office now = offices[i];
+            if (time <= now.start) {
+                answer++;
+                time = now.end;
             }
         }
-        System.out.println(max);
+
+        System.out.println(answer);
     }
 }
