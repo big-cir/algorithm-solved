a97cf8a968f45e3825858f62640c1a9c7e67ab99
dae won <99483390+big-cir@users.noreply.github.com>
2024-11-30 00:22:26 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,9 +1,8 @@
 -- 코드를 작성해주세요
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPERS d
-where d.skill_code & (
-    select sum(s.code)
-    from SKILLCODES s
-    where s.CATEGORY = 'Front End'
-)
+join SKILLCODES s 
+on (d.skill_code & s.code) > 0
+where s.CATEGORY = 'Front End'
+GROUP BY ID, EMAIL, FIRST_NAME, LAST_NAME
 order by ID;
