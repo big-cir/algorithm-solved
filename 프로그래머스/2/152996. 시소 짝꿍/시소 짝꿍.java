ce002fa885a477bff0948f5b000c44b938a82ed4
dae won <99483390+big-cir@users.noreply.github.com>
2025-01-29 23:11:33 +0900

    [level 2] Title: 시소 짝꿍, Time: 184.27 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,29 +1,36 @@
-// 완탐 : O(N) = 백억 (x);
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
-    static int[] sits = new int[] {2, 3, 4};
-    
     public long solution(int[] weights) {
         long answer = 0;
-
         Arrays.sort(weights);
+        
         int cnt = 0;
         for (int i = 0; i < weights.length - 1; i++) {
+            // 같은 weight가 연속되는 경우 최적화
             if (i > 0 && weights[i] == weights[i - 1]) {
                 cnt--;
                 answer += cnt;
                 continue;
             }
-
+            
             cnt = 0;
+            // 현재 weight와 짝이 될 수 있는 다음 weight들 확인
             for (int j = i + 1; j < weights.length; j++) {
-                if (weights[i] == weights[j]) cnt++;
-                else if (weights[i] * 4 == weights[j] * 3) cnt++;
-                else if (weights[i] * 3 == weights[j] * 2) cnt++;
-                else if (weights[i] * 2 == weights[j]) cnt++;
+                int w1 = weights[i];
+                int w2 = weights[j];
+                
+                // 가능한 모든 비율 확인
+                if (w1 == w2 || 
+                    w1 * 4 == w2 * 3 || 
+                    w1 * 3 == w2 * 2 || 
+                    w1 * 2 == w2) {
+                    cnt++;
+                }
             }
             answer += cnt;
         }
+        
         return answer;
     }
 }
