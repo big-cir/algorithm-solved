5d4749d8f089f78709029d6afe07fede59e5086c
dae won <99483390+big-cir@users.noreply.github.com>
2025-02-06 14:54:06 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 124068 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, zeroTomato, answer = 0;
-    static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
-    static int[][] map;
-    static Queue<int[]> queue;
 
+    static int N, M;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,45 +14,62 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         map = new int[N][M];
 
-        queue = new LinkedList<>();
-        zeroTomato = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] ch = new int[N][M];
+        int dCnt = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                int val = Integer.parseInt(st.nextToken());
-                map[i][j] = val;
-                if (val == 1) queue.offer(new int[] {i, j});
-                if (val == 0) zeroTomato++;
+                int tomato = Integer.parseInt(st.nextToken());
+                map[i][j] = tomato;
+
+                if (tomato == 1) {
+                    queue.offer(new int[]{i, j});
+                    ch[i][j] = 1;
+                }
+
+                if (tomato == 0) dCnt++;
             }
         }
 
-        if (zeroTomato == 0) {
-            System.out.println("0");
-        } else {
-            bfs();
-            if (zeroTomato != 0) {
-                System.out.println("-1");
-            } else {
-                System.out.println(answer - 1);
-            }
-        }
-    }
-
-    private static void bfs() {
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+            answer = Math.max(answer, map[x][y]);
 
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == 0) {
-                    map[nx][ny] = map[cur[0]][cur[1]] + 1;
-                    answer = Math.max(answer, map[nx][ny]);
-                    zeroTomato--;
-                    queue.offer(new int[] {nx, ny});
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (ch[nx][ny] == 0 && map[nx][ny] == 0) {
+                        ch[nx][ny] = 1;
+                        map[nx][ny] = map[x][y] + 1;
+                        queue.offer(new int[] {nx, ny});
+                    }
                 }
             }
         }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    answer = 0;
+                }
+
+                if (map[i][j] == 1) {
+                    dCnt--;
+                }
+            }
+        }
+
+        if (dCnt == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer - 1);
+        }
     }
+
 }
