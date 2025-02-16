cb380787298052ece882c1d3c5f0dd1f292cadec
dae won <99483390+big-cir@users.noreply.github.com>
2025-02-16 19:44:57 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,9 @@
-SELECT ITEM_ID, ITEM_NAME, RARITY
-FROM ITEM_INFO
-WHERE ITEM_ID IN (SELECT A.ITEM_ID
-                  FROM ITEM_INFO I, ITEM_TREE A
-                WHERE I.ITEM_ID = A.PARENT_ITEM_ID AND I.RARITY = 'RARE')
-ORDER BY ITEM_ID DESC;
+select ITEM_ID, ITEM_NAME, RARITY
+from ITEM_INFO
+where ITEM_ID in (
+    select s1.ITEM_ID
+    from ITEM_TREE s1
+    join ITEM_INFO s2 on s2.ITEM_ID = s1.PARENT_ITEM_ID and s2.RARITY = 'RARE'
+    where s1.PARENT_ITEM_ID is not null
+)
+order by ITEM_ID desc;
