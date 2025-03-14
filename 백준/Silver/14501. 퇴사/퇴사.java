0318a78225205db4640fd52bc3cf718f9043b569
dae won <99483390+big-cir@users.noreply.github.com>
2025-03-14 20:45:31 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,30 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
+    static int N;
+    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-
-        int[][] schedule = new int[N + 1][2];
-        for (int i = N; i > 0; i--) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            schedule[i][0] = Integer.parseInt(st.nextToken());
-            schedule[i][1] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        for (int i = 1; i < dp.length; i++) {
-            if (schedule[i][0] > i) {
-                dp[i] = dp[i - 1];
+        int[] dp = new int[N + 2];
+        for (int i = N; i >= 1; i--) {
+            int day = arr[i - 1][0];
+            int cost = arr[i - 1][1];
+
+            if (i + day - 1 <= N) {
+                dp[i] = Math.max(dp[i + 1], dp[i + day] + cost);
             } else {
-                dp[i] = Math.max(dp[i - 1], schedule[i][1] + dp[i - schedule[i][0]]);
+                dp[i] = dp[i + 1];
             }
         }
 
-        System.out.println(dp[N]);
+        System.out.println(dp[1]);
     }
 }
