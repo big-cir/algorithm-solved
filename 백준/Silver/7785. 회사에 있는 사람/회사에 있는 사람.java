6008a9f0c84fffb18e15bc6bbbccfe45155f71a9
dae won <99483390+big-cir@users.noreply.github.com>
2025-04-08 23:23:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1164 ms, Memory: 59652 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
         Set<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            String state = st.nextToken();
+            String[] strs = br.readLine().split(" ");
+            String name = strs[0];
+            String status = strs[1];
 
-            if (state.equals("enter")) {
-                set.add(name);
-            } else {
+            if (status.equals("leave")) {
                 set.remove(name);
+            } else {
+                set.add(name);
             }
         }
 
-        List<String> answer = new ArrayList<>(set);
-        answer.sort(Collections.reverseOrder());
-        for (String x : answer) {
-            System.out.println(x);
+        List<String> list = new ArrayList<>(set);
+        list.sort(Collections.reverseOrder());
+        for (String str : list) {
+            System.out.println(str);
         }
     }
 }
