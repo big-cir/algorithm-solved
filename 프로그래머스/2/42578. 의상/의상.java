0daffe4046b9b5a80a40b02c10818cf1262c9f7a
dae won <99483390+big-cir@users.noreply.github.com>
2025-04-08 23:22:28 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 // 조합론: 0개 이상 n개 미만 선택하지 않는 경우의 수 (곱의 법칙)
 //  -> 0...n개까지 선택 가능한 경우의 수 (n + 1)
-
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
