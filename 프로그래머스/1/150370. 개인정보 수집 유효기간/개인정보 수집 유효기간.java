59ec0c183bb426711326153650f5ca999406bae5
dae won <99483390+big-cir@users.noreply.github.com>
2025-05-07 23:40:30 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 28.07 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,66 +1,72 @@
 import java.util.*;
 
+// ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
 class Solution {
-    
-    static List<Integer> answer;
-    static Map<String, Integer> term;
-    
     public int[] solution(String today, String[] terms, String[] privacies) {
-        term = new HashMap<>();
-        for (String str : terms) {
-            String[] arr = str.split(" ");
-            term.put(arr[0], Integer.parseInt(arr[1]));
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < terms.length; i++) {
+            String term = terms[i];
+            String[] strs = term.split(" ");
+            map.put(strs[0], Integer.parseInt(strs[1]));
         }
         
-        today = today.replaceAll("\\.", "");
-        int todayValue = Integer.parseInt(today);
-        
-        int idx = 1;
-        answer = new ArrayList<>();
-        for (String str : privacies) {
-            String[] info = str.split(" ");
-            String day = info[0];
-            String type = info[1];
-            String[] dayInfo = day.split("\\.");
+        List<Integer> result = new ArrayList<>();
+        int[] numsToday = numsDate(today);
+        String comp1 = getDate(numsToday);
+        for (int i = 0; i < privacies.length; i++) {
+            String privacy = privacies[i];
+            String[] dateRuleStrs = privacy.split(" ");
+            int[] startDate = numsDate(dateRuleStrs[0]);
+            int ruleMonth = map.get(dateRuleStrs[1]);
             
-            int y = Integer.parseInt(dayInfo[0]);
-            int m = Integer.parseInt(dayInfo[1]);
-            int d = Integer.parseInt(dayInfo[2]);
+            int expiredYear = startDate[0] + ruleMonth / 12;
+            int expiredMonth = startDate[1] + ruleMonth % 12;
+            int expiredDay = startDate[2] - 1;
             
-            String expired = "";
-            
-            d--;
-            if (d == 0) {
-                d = 28;
-                m--;
+            if (expiredDay == 0) {
+                expiredMonth -= 1;
+                expiredDay = 28;
                 
-                if (m == 0) {
-                    m = 12;
-                    y--;
-                }
+                if (expiredMonth == 0) {
+                    expiredYear -= 1;
+                    expiredMonth = 12;
+                } 
             }
             
-            m += term.get(type);
-            if (m > 12) {
-                while (m > 12) {
-                    m -= 12;
-                    y++;
-                }
+            if (expiredMonth >= 13) {
+                expiredMonth = expiredMonth - 12;
+                expiredYear += 1;
             }
             
-            expired += y;
-            
-            if (m < 10) expired += "0" + m;
-            else expired += m;
-            
-            if (d < 10) expired +="0" + d;
-            else expired += d;
-            
-            int expiredValue = Integer.parseInt(expired);
-            if (expiredValue < todayValue) answer.add(idx);
-            idx++;
+            int[] expiredDate = new int[] {expiredYear, expiredMonth, expiredDay};
+            String comp2 = getDate(expiredDate);
+            if (Integer.parseInt(comp2) < Integer.parseInt(comp1)) result.add(i + 1);
         }
         
-        return answer.stream().mapToInt(x -> x).toArray();
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) answer[i] = result.get(i);
+        return answer;
+    }
+    
+    private int[] numsDate(String today) {
+        String[] strs = today.split("\\.");
+        int[] nums = new int[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+        
+        return nums;
+    }
+    
+    private String getDate(int[] date) {
+        String strDate = String.valueOf(date[0]);
+        
+        if (date[1] < 10) strDate += ("0" + date[1]);
+        else strDate += date[1];
+        
+        if (date[2] < 10) strDate += ("0" + date[2]);
+        else strDate += date[2];
+                                      
+        return strDate;
     }
 }
