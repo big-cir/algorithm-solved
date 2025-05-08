8a4630464144d9aaeabbe8eab94dad9593cee5be
dae won <99483390+big-cir@users.noreply.github.com>
2025-05-08 19:00:11 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.36 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,21 +1,36 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    private int[] dx = {-1, 0, 1, 0};
-    private int[] dy = {0, 1, 0, -1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        for (int i = 0; i < places.length; i++) {
-            char[][] chars = makeArr(places[i]);
-            int cnt = 0;
+        int n = places.length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            String[] place = places[i];
+            int m = place.length;
+            
+            List<int[]> pos = new ArrayList<>();
+            char[][] board = new char[m][place[0].length()];
+            for (int j = 0; j < m; j++) {
+                board[j] = place[j].toCharArray();
+                
+                for (int k = 0; k < place[j].length(); k++) {
+                    if (board[j][k] == 'P') pos.add(new int[] {j, k});
+                }
+            }
+            
             boolean flag = true;
-            for (int j = 0; j < chars.length; j++) {
-                for (int k = 0; k < chars[0].length; k++) {
-                    if (chars[j][k] == 'P') {
-                        cnt++;
-                        if (!bfs(j, k, chars)) {
+            for (int j = 0; j < pos.size() - 1; j++) {
+                int[] start = pos.get(j);
+                
+                if (!flag) break;
+                
+                for (int k = j + 1; k < pos.size(); k++) {
+                    int[] dst = pos.get(k);
+                    if (!isManhattanDis(start, dst)) {
+                        if (!bfs(start, dst, board)) {
                             flag = false;
                             break;
                         }
@@ -23,51 +38,48 @@ class Solution {
                 }
             }
 
-            if (cnt == 0 || flag) answer[i] = 1;
-            else {
-                answer[i] = 0;
-            }
+            if (flag) answer[i] = 1;
         }
-
+        
         return answer;
     }
-
-    private boolean bfs(int x, int y, char[][] chars) {
-        int[][] ch = new int[chars.length][chars[0].length];
-        Queue<int[]> queue = new LinkedList<>();
-        ch[x][y] = 1;
-        queue.offer(new int[] {x, y, 0});
-
+    
+    private boolean isManhattanDis(int[] start, int[] dst) {
+        return Math.abs(start[0] - dst[0]) + Math.abs(start[1] - dst[1]) > 2;
+    }
+    
+    private boolean bfs(int[] start, int[] dst, char[][] board) {
+        int[][] ch = new int[board.length][board[0].length];
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {start[0], start[1], 0});
+        ch[start[0]][start[1]] = 1;
+        
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
-            int curDis = now[2];
-
+            int x = now[0];
+            int y = now[1];
+            int dis = now[2];
+            
+            if (x == dst[0] && y == dst[1]) {
+                if (dis <= 2) return false;
+                break;
+            }
+            
             for (int i = 0; i < 4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-
-                if (isValid(nx, ny, ch) && chars[nx][ny] == 'O') {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (isValid(nx, ny) && ch[nx][ny] == 0 &&  (board[nx][ny] == 'O' || board[nx][ny] == 'P')) {
                     ch[nx][ny] = 1;
-                    queue.offer(new int[] {nx, ny, curDis + 1});
-                }
-
-                if (isValid(nx, ny, ch) && chars[nx][ny] == 'P' && curDis < 2) {
-                    return false;
+                    queue.offer(new int[] {nx, ny, dis + 1});
                 }
             }
         }
+        
         return true;
     }
-
-    private boolean isValid(int nx, int ny, int[][] ch) {
-        return nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && ch[nx][ny] == 0;
-    }
-
-    private char[][] makeArr(String[] str) {
-        char[][] chars = new char[5][5];
-        for (int i = 0; i < 5; i++) {
-            chars[i] = str[i].toCharArray();
-        }
-        return chars;
+    
+    private boolean isValid(int x, int y) {
+        return x >= 0 && x < 5 && y >= 0 && y < 5;
     }
 }
