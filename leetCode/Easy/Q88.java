a989d7f8e45887909f51a3f4ab3630186cc3329a
dae won <99483390+big-cir@users.noreply.github.com>
2025-10-06 14:23:05 +0900

    Q88.java

--- leetCode/Easy/Q88.java
+++ leetCode/Easy/Q88.java
@@ -1,3 +1,4 @@
+// so1
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for (int i = 0; i < n; i++) {
@@ -5,5 +6,45 @@ class Solution {
         }
 
         Arrays.sort(nums1);
+    }
+}
+
+// sol2
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+
+        int size = n + m;
+        int[] tmp = new int[size];
+        sort(0, size - 1, nums1, tmp);
+    }
+
+    private void sort(int start, int end, int[] nums1, int[] tmp) {
+        if (start >= end) return;
+        
+        int mid = start + (end - start) / 2;
+        sort(start, mid, nums1, tmp);
+        sort(mid + 1, end, nums1, tmp);
+
+        for (int i = start; i <= end; i++) {
+            tmp[i] = nums1[i];
+        }
+
+        int afterStart = start;
+        int afterEnd = mid + 1;
+        int mergeArrIdx = start;
+        while (afterStart <= mid && afterEnd <= end) {
+            if (tmp[afterStart] <= tmp[afterEnd]) {
+                nums1[mergeArrIdx++] = tmp[afterStart++];
+            } else {
+                nums1[mergeArrIdx++] = tmp[afterEnd++];
+            }
+        }
+
+        while (afterStart <= mid) {
+            nums1[mergeArrIdx++] = tmp[afterStart++];
+        }
     }
 }
