6c967ae79d3f9e68b000a75ada5c3571dd4dea5b
dae won <99483390+big-cir@users.noreply.github.com>
2026-02-25 20:58:23 +0900

    Accounts Merge

--- leetCode/Medium/Q721.java
+++ leetCode/Medium/Q721.java
@@ -42,3 +42,69 @@ class Solution {
         return answer;
     }
 }
+
+// Union-Find
+class Solution {
+
+    static int[] parent;
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        int n = accounts.size();
+        parent = new int[n];
+        for (int i = 1; i < n; i++) {
+            parent[i] = i;
+        }
+
+        Map<String, Integer> emailToIdx = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            List<String> account = accounts.get(i);
+            for (int j = 1; j < account.size(); j++) {
+                String email = account.get(j);
+                if (emailToIdx.containsKey(email)) {
+                    union(i, emailToIdx.get(email));
+                } else {
+                    emailToIdx.put(email, i);
+                }
+            }
+        }
+        
+        Map<Integer, TreeSet<String>> result = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int index = find(i);
+            result.computeIfAbsent(index, k -> new TreeSet<>());
+
+            List<String> account = accounts.get(i);
+            for (int j = 1; j < account.size(); j++) {
+                String email = account.get(j);
+                result.get(index).add(email);
+            }
+        }
+
+        List<List<String>> answer = new ArrayList<>();
+        for (int index : result.keySet()) {
+            List<String> sub = new ArrayList<>();
+            String name = accounts.get(index).get(0);
+            sub.add(name);
+            sub.addAll(result.get(index));
+
+            answer.add(sub);
+        }
+
+        return answer;
+    }
+
+    private void union(int a, int b) {
+        int na = find(a);
+        int nb = find(b);
+        parent[na] = nb;
+    }
+
+    private int find(int x) {
+        if (parent[x] != x) {
+            int nx = parent[x];
+            parent[x] = find(nx);
+        }
+
+        return parent[x];
+    }
+}
